568877a1ac70e83e48660a1b31e7207bddacfd8f
Colton <colton@sendpepper.com>
2015-08-31 21:17:32 -0700

    first set of files

---
+++ myscript.js
@@ -0,0 +1,9 @@
+
+if (document.getElementById("ussr-chrome-sidebar").style.visibility == "hidden") {
+
+	document.getElementById("ussr-chrome-sidebar").style.visibility = "visible";
+	document.getElementById("ussr-chrome-panels").style.left = "175px";
+} else {
+	document.getElementById("ussr-chrome-sidebar").style.visibility = "hidden";
+	document.getElementById("ussr-chrome-panels").style.left = "0px";
+}
